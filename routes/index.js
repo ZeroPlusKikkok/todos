821c873e8ee3660e595c1378c6045b827391179b
EN01 - สันติ วิไลพล <EN01@ASIANGROUP.CO.TH>
2021-07-26 15:41:05 +0700

    First Commit ReactJS

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'NeverStopLearning' });
+});
+
+const testJSON = [
+  {
+    name: 'Santi',
+    username: 'ZeroPlusCode',
+  },
+  {
+    name: 'Piyawat',
+    usernamne: 'Drunkman'
+  }
+];
+
+router.get('/sendjson', (req, res, next) => {
+  res.json(testJSON)
+});
+
+module.exports = router;
